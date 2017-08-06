f6579193626fc9d4aa54205fa5e868046d9da93f
xxlf <night888888@126.com>
2017-08-06 12:36:05 +0800

    modify conf

--- conf/conf.sh
+++ conf/conf.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "start up"
+echo "##########################################"
+
+echo "satrt fdfsDFS..."
+sudo fdfs_trackerd ./fdfsDFS/tracker.conf
+
+sudo fdfs_storaged ./fdfsDFS/storage.conf
 
